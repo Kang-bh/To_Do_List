aee3e31b7825ae27c32ed099f15dd0954dcf464d
BH-Kang <kabyho3513@gmail.com>
2022-01-19 01:16:18 +0900

    #03 Install mongoose and Connect MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://KBH:skoo1293@todolist.h7at6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+// MongoDB PW 특수문자 X
+// 6.0 이상 부터는 기본적용  
+//useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
