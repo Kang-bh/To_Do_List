172bc1e0ee59811a52421a7b90a40c157ee38518
BH-Kang <kabyho3513@gmail.com>
2022-01-19 23:50:35 +0900

    #05 Register Function

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+
+const {User} = require("./models/user")
+
+// application/x-www-form-urlencoded 형태 데이터 분석
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json 형식 분석
+app.use(bodyParser.json());
+
+
 mongoose.connect('mongodb+srv://KBH:skoo1293@todolist.h7at6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 // MongoDB PW 특수문자 X
 // 6.0 이상 부터는 기본적용  
@@ -9,8 +19,25 @@ mongoose.connect('mongodb+srv://KBH:skoo1293@todolist.h7at6.mongodb.net/myFirstD
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+  // 회원가입시 필요한 정보들 클라이언트에서 가져오면 DB에 넣어주는 것
+  // 인스턴스 생성
+  // bodyParser 이용해서 받아오는 것
+  const user = new User(req.body) // req.body 안에 json 형식의 아이디  { id :, password: }
+  
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  }) // mongodb method user모델에 정보 저장
 })
 
 app.listen(port, () => {
