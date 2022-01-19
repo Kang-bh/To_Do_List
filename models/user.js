1f909abae5c82f9f97ce86605c1d5aa05e7feb70
BH-Kang <kabyho3513@gmail.com>
2022-01-20 00:55:00 +0900

    #07 Encrypt Password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10
 
 const userSchema = mongoose.Schema({
     name:{
@@ -33,6 +35,26 @@ const userSchema = mongoose.Schema({
     tokenExp: { //token 유효기간
         type: Number
     }
+})
+
+// index.js 의 save 전에 할 일
+userSchema.pre('save', function(next){
+    var user = this;
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                // Store hash in your password DB.
+                if(err) return next(err)
+                user.password = hash // hash 된 비밀번호로 변경
+                next()
+            });
+        });
+    } else {
+        // 다른 거 변경 시
+        next()
+    }
+    // 비밀번호 암호화 시키기
+    
 })
 
 const User = mongoose.model('User', userSchema)
